722ffa3cc8a044d7fc6e93cf132d5f8f9032a8ad
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-05-14 17:17:14 +0300

    UP

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
 import templateCardProduct from './template/CardProduct.hbs';
 import products from './Product/products';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import axios from 'axios';
+
+require('dotenv').config();
+console.log(process.env.TOKEN);
 
 
+// const token = process.env.TOKEN;
+// console.log('token:', token);
+// const chatid = process.env.CHATID;
+// console.log('chatid:', chatid);
 
-const refs = {
-  catalog: document.querySelector('.catalog-list'),
-};
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+});
 
-refs.catalog.insertAdjacentHTML('beforeend', templateCardProduct(products));
+document
+  .querySelector('.catalog-list')
+  .insertAdjacentHTML('beforeend', templateCardProduct(products));
+
+document
+  .querySelector('#fotter-form')
+  .addEventListener('submit', onSendMessageTelegram);
+
+function onSendMessageTelegram(e) {
+  e.preventDefault();
+
+  let numberPhone = `<b>!!Заявка с сайта(футтер)!!</b>\nНомер телефона: <b>${this.phone.value}</b>`;
+
+  axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+    parse_mode: 'html',
+    chat_id: chatid,
+    text: numberPhone,
+  });
+}
