bc947ec1094ccab7d2d7257c42064198872ab549
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-05-14 17:58:04 +0300

    UP

--- src/index.js
+++ src/index.js
@@ -22,10 +22,15 @@ function onSendMessageTelegram(e) {
   e.preventDefault();
 
   let numberPhone = `<b>!!Заявка с сайта(футтер)!!</b>\nНомер телефона: <b>${this.phone.value}</b>`;
+
+  const token =
+    process.env.TOKEN || '6150030403:AAEvKg9VqjBFZCz59AJ6Bjb3UQ_mZ1DwT5w';
+  const chat = process.env.CHATID || '-1001872926354';
+
   console.log(process.env.TOKEN);
-  axios.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`, {
+  axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
     parse_mode: 'html',
-    chat_id: process.env.CHATID,
+    chat_id: chat,
     text: numberPhone,
   });
 }
